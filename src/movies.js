3e7e4c8f63d44293ccd1abc78bdea10f4f613672
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-09-22 23:58:35 +0300

    edit iteration 8

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,19 @@
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
-// How could you "clean" a bit this array and make it unified (without duplicates)?
 
 function getAllDirectors(array) {
-  let listOfDirectors = array.map((movie) => {
-    return movie.director;
-  });
-  return listOfDirectors;
+  const directorsArray = array.map((movie) => movie.director);
+  return directorsArray;
 }
+
+// _Bonus_1.1: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// How could you "clean" a bit this array and make it unified (without duplicates)?
+
+// function getAllDirectors(array) {
+//   const directorsArrayUnique = array
+//     .map((movie) => movie.director)
+//     .filter((elm, index, array) => array.indexOf(elm) === index);
+//   return directorsArrayUnique;
+// }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(array) {
@@ -46,7 +52,8 @@ function dramaMoviesScore(array) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(array) {
-  let orderMoviesByYear = Object.assign([], array);
+  // let orderMoviesByYear = Object.assign([], array); -> ALTERNATIVE
+  const orderMoviesByYear = [...array];
 
   orderMoviesByYear.sort((a, b) => {
     if (a.year === b.year) {
@@ -73,27 +80,75 @@ function orderAlphabetically(array) {
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(array) {
-  let movieTimeInMinutes = Object.assign([], array);
+  let movieTimeInMinutes = [...array];
 
-  if (movieTimeInMinutes.length === 0) {
+  if (array.length === 0) {
     return null;
   }
 
+  let hours = 0;
+  let minutes = 0;
+  let newTime = 0;
+
   movieTimeInMinutes.map((movie) => {
-    let hours = movie.duration[0] * 60;
-    let minutes = 0;
-    if (movie.duration.includes('min')) {
-      minutes = parseInt(movie.duration.slice(3, 5));
+    if (movie.duration[0] === 0) {
+      hours = 0;
+    } else {
+      hours = movie.duration[0] * 60;
     }
 
-    movie.duration = hours + minutes;
+    if (movie.duration.includes('min')) {
+      minutes = parseInt(movie.duration.slice(3, -3));
+    } else {
+      minutes = 0;
+    }
+
+    newTime = hours + minutes;
+    movie.duration = newTime;
+
     return movie.duration;
   });
   return movieTimeInMinutes;
 }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  let listOfYears = [...array];
+  let arrayOfAllYears = [];
+
+  listOfYears.forEach((movie) => {
+    if (!arrayOfAllYears.includes(movie.year)) {
+      arrayOfAllYears.push(movie.year);
+    }
+  });
+
+  let bestYear;
+  let bestAverage = 0;
+
+  arrayOfAllYears.forEach((year) => {
+    let listOfMoviesPerYear = array.filter((movie) => {
+      return movie.year === year;
+    });
+
+    let averageScore = listOfMoviesPerYear.reduce(
+      (acumulator, currentValue) =>
+        acumulator +
+        Math.round((currentValue.score / listOfMoviesPerYear.length) * 100) /
+          100,
+      0
+    );
+
+    if (averageScore > bestAverage) {
+      bestAverage = averageScore;
+      bestYear = year;
+    }
+  });
+
+  return `The best year was ${bestYear} with an average score of ${bestAverage}`;
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
